60258f073f1d1c76e11c27b1eac0d16fed2fad67
riya-kaurav <kauravriya665@gmail.com>
2025-12-14 17:02:29 +0530

    estabilsh db connection

---
+++ lib/db.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGOODB_URL!;
+
+if(!MONGODB_URI) {
+    throw new Error("Please define mongodb");
+
+}
+
+let cached = global.mongoose;
+if(!cached) {
+    cached = global.mongoose = { conn : null , promise: null};
+    
+}
+
+export async function connectToDatabase() {
+    if(cached.conn) {
+        return cached.conn
+    }
+
+    if(!cached.promise) {
+    const opts = {
+        bufferCommands: true, // its use ?
+        maxPoolSize: 10 
+    };
+    
+
+    cached.promise =  mongoose
+    .connect(MONGODB_URI , opts)
+    .then(() => mongoose.connection);
+
+    }
+
+    try{
+         cached.conn = await cached.promise
+    }
+    catch(error) {
+        cached.promise = null 
+        throw new Error("Check database file");
+
+    }
+  return cached.conn ;
+}
